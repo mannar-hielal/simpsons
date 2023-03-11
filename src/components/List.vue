e99b32b3d3642075ffb79eb95aeb13dc5d9a8884
mannar.hielal <adminwithu@gmail.com>
2023-03-11 18:20:06 +0100

    Replace with html5 semantic markup

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,23 +1,31 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <h1 class="text-xl mb-8">Simpson Quotes</h1>
-  <!-- <SortButton direction="up" text="Sort ascending" />
-  <SortButton direction="down" text="Sort descending" /> -->
-  <div
-    v-for="quote in simpsons"
-    :key="quote"
-    class="bg-gradient-to-br from-gray-200 rounded-lg shadow w-64"
-  >
-    <div class="flex-1 flex flex-col p-8">
-      <img
-        class="w-32 h-32 flex-shrink-0 mx-auto rounded-full shadow-xl bg-white"
-        :src="quote.image"
-        alt="image of quote.character"
-      />
-      <h3 class="mt-6 text-gray-900 text-xs">
-        {{ quote.quote }}
-      </h3>
-    </div>
+  <div>
+      <h1 class="text-xl mb-8">Simpson Quotes</h1>
+      <!-- <SortButton direction="up" text="Sort ascending" />
+      <SortButton direction="down" text="Sort descending" /> -->
+      <section>
+        <div class="row">
+          <!-- Use semantic markup: article, figure-->
+          <article
+          v-for="quote in simpsons"
+          :key="quote"
+          class="bg-gradient-to-br from-gray-200 rounded-lg shadow w-64"
+        >
+            <figure class="flex-1 flex flex-col p-8">
+              <img 
+              :src="quote.image"
+              :alt="`image of ${quote.character}`"
+              class="w-32 h-32 flex-shrink-0 mx-auto rounded-full shadow-xl bg-white"
+              >
+              <figcaption class="mt-6 text-gray-900 text-xs">
+                  <q>{{ quote.quote }}</q>
+                  <footer>—{{ quote.character }}</footer>
+              </figcaption>
+            </figure>
+        </article>
+        </div>
+      </section>
   </div>
 </template>
 
